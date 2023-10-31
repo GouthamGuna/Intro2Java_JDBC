7568560b02c950df2066decc09ffe6c76039a099
Goutham <ggowthamsankar@gmail.com>
2023-10-31 11:36:44 +0530

    Update DemoHibernateApplicationTest.java

--- demo_hibernate/src/test/java/in/dev/gmsk/DemoHibernateApplicationTest.java
+++ demo_hibernate/src/test/java/in/dev/gmsk/DemoHibernateApplicationTest.java
@@ -1,5 +1,13 @@
+package in.dev.gmsk;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoHibernateApplicationTest {
 
+    @Test
+    void mainTest() {
+        assertTrue(true);
+    }
 }
