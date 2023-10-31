4dbf83be97a91414bfc02bc388722833665a524b
Goutham <ggowthamsankar@gmail.com>
2023-10-31 11:34:42 +0530

    initial commit

---
+++ demo_hibernate/src/main/java/in/dev/gmsk/DemoHibernateApplication.java
@@ -0,0 +1,19 @@
+package in.dev.gmsk;
+
+import java.io.PrintWriter;
+
+public class DemoHibernateApplication {
+
+    public static void main(String[] args) {
+
+        try {
+            PrintWriter printWriter =
+                    new PrintWriter(System.out);
+
+            printWriter.println("Hello Lunar *)");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
