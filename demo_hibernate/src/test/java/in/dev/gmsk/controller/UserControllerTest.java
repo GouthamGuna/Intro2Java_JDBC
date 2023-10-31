48c66dee534324aa8b192a386ac2afea270f61d0
Goutham <ggowthamsankar@gmail.com>
2023-10-31 11:37:02 +0530

    Update UserControllerTest.java

--- demo_hibernate/src/test/java/in/dev/gmsk/controller/UserControllerTest.java
+++ demo_hibernate/src/test/java/in/dev/gmsk/controller/UserControllerTest.java
@@ -1,5 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.dev.gmsk.controller;
+
+import in.dev.gmsk.model.User;
+import org.junit.jupiter.api.Test;
 
 class UserControllerTest {
 
+    @Test
+    void saveUserTest() {
+
+        User user = new User();
+        user.setId(1);
+        user.setName("Gowtham Sankar Gunasekaran");
+        user.setPassword("PWD@123");
+        user.setEmail("gowthamsankar@gmsk.in");
+
+        int saveUser = UserController.saveUser(user);
+
+        System.out.println("saveUser = " + saveUser);
+    }
 }
