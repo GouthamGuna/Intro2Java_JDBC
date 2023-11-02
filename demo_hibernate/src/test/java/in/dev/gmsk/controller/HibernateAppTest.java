63a9218a77551d2d12cea6154937ef5770ac46f2
Goutham <ggowthamsankar@gmail.com>
2023-11-02 13:05:25 +0530

    Update HibernateAppTest.java

--- demo_hibernate/src/test/java/in/dev/gmsk/controller/HibernateAppTest.java
+++ demo_hibernate/src/test/java/in/dev/gmsk/controller/HibernateAppTest.java
@@ -1,5 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package in.dev.gmsk.controller;
+
+import in.dev.gmsk.model.Address;
+import in.dev.gmsk.model.User;
+import org.junit.jupiter.api.Test;
 
 class HibernateAppTest {
 
+    @Test
+    void mainMethodTest() {
+
+        User user = new User(2427, "Gowtham Sankar",
+                "dummy@PWD", "dummy@gmsk.in");
+
+        Address address = new Address("Electronic City", "2427",
+                "Bangalore", "Karnataka", "India", "12345");
+
+        int i = HibernateApp.mainMethod(user, address);
+
+        System.out.println("i = " + i);
+    }
 }
