109c33e1991e9fcd5fd95a3f1dc879c4f2d72762
Goutham <ggowthamsankar@gmail.com>
2023-11-02 13:03:47 +0530

    Update Address.java

--- demo_hibernate/src/main/java/in/dev/gmsk/model/Address.java
+++ demo_hibernate/src/main/java/in/dev/gmsk/model/Address.java
@@ -1,4 +1,28 @@
 package in.dev.gmsk.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@Setter
+@Getter
 public class Address {
+
+    private String addressLine1;
+
+    private String addressLine2;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String zipCode;
 }
