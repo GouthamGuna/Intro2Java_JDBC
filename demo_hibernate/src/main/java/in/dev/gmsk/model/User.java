bc79f8c283717dfbc3b6cccfaca90c6064a2d2da
Goutham <ggowthamsankar@gmail.com>
2023-11-02 13:04:10 +0530

    Update User.java

--- demo_hibernate/src/main/java/in/dev/gmsk/model/User.java
+++ demo_hibernate/src/main/java/in/dev/gmsk/model/User.java
@@ -1,15 +1,15 @@
 package in.dev.gmsk.model;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.Embeddable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
-@Entity
+@Embeddable
 public class User {
 
     private int id;
